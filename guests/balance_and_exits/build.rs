8edb7b6cd4f953493f67f6fb9145097264e07a49
Willem Olding <willemolding@gmail.com>
2025-01-17 18:16:18 +1100

    ignore error when generating solidity files

--- guests/balance_and_exits/build.rs
+++ guests/balance_and_exits/build.rs
@@ -43,5 +43,5 @@ fn main() {
         .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
         .with_elf_sol_path(SOLIDITY_ELF_PATH);
 
-    generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
+    let _ = generate_solidity_files(guests.as_slice(), &solidity_opts);
 }
