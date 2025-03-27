ffc3e416259627502a70a792e8ee4850b5879d19
Willem Olding <willemolding@gmail.com>
2025-03-27 14:34:04 +1300

    Update to RISC Zero v2 (#7)
    * bump versions and make changes for v2
    
    * tests passing v2!

--- guests/balance_and_exits/build.rs
+++ guests/balance_and_exits/build.rs
@@ -14,7 +14,7 @@
 
 use std::{collections::HashMap, env};
 
-use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
+use risc0_build::{embed_methods_with_options, GuestOptionsBuilder};
 use risc0_build_ethereum::generate_solidity_files;
 
 // Paths where the generated Solidity files will be written.
@@ -26,20 +26,13 @@ const SOLIDITY_ELF_PATH: &str =
     concat!(env!("CARGO_MANIFEST_DIR"), "/../../contracts/tests/Elf.sol");
 
 fn main() {
-    // Builds can be made deterministic, and thereby reproducible, by using Docker to build the
-    // guest. Check the RISC0_USE_DOCKER variable and use Docker to build the guest if set.
-    println!("cargo:rerun-if-env-changed=RISC0_USE_DOCKER");
-    let use_docker = env::var("RISC0_USE_DOCKER").ok().map(|_| DockerOptions {
-        root_dir: Some("../".into()),
-    });
-
     // Generate Rust source files for the methods crate.
     let guests = embed_methods_with_options(HashMap::from([(
         "balance_and_exits",
-        GuestOptions {
-            features: Vec::new(),
-            use_docker: use_docker.clone(),
-        },
+        GuestOptionsBuilder::default()
+            .features(Vec::new())
+            .build()
+            .unwrap(),
     )]));
 
     // Generate Solidity source files for use with Forge.
