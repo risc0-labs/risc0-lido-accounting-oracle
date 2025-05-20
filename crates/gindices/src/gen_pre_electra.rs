b846f6db74e8043b9baf6c674e13064e605b9ecb
Willem Olding <willemolding@gmail.com>
2025-05-21 10:44:51 +1200

    fmt generated code

--- crates/gindices/src/gen_pre_electra.rs
+++ crates/gindices/src/gen_pre_electra.rs
@@ -1,7 +1,21 @@
-pub fn slot() -> u64 { 34 }
-pub fn validator_count() -> u64 { 87 }
-pub fn state_roots_base() -> u64 { 311296 }
-pub fn historical_summaries_base() -> u64 { 1979711488 }
-pub fn validator_balance_base() -> u64 { 24189255811072 }
-pub fn validator_withdrawal_credentials_base() -> u64 { 756463999909889 }
-pub fn validator_exit_epoch_base() -> u64 { 756463999909894 }
+pub fn slot() -> u64 {
+    34
+}
+pub fn validator_count() -> u64 {
+    87
+}
+pub fn state_roots_base() -> u64 {
+    311296
+}
+pub fn historical_summaries_base() -> u64 {
+    1979711488
+}
+pub fn validator_balance_base() -> u64 {
+    24189255811072
+}
+pub fn validator_withdrawal_credentials_base() -> u64 {
+    756463999909889
+}
+pub fn validator_exit_epoch_base() -> u64 {
+    756463999909894
+}
